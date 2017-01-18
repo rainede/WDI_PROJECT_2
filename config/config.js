7fb60f34cafcb894235bdf5a1b5017e45cce1398
rainede <rainey@techgiver.com>
2017-01-18 10:27:21 +0000

    changing api URL to heroku url

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB_URI||  'mongodb://localhost/WDI_PROJECT_2',
   secret: process.env.SECRET || 'gosh this is so secret... shhh...',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://healthycommute.herokuapp.com/api',
   //tflCameras: 'https://s3-eu-west-1.amazonaws.com/tfl.pub/Jamcams/jamcams-camera-list.xml'
   tflCameras: 'http://tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/',
   googleMapsAPIKey: process.env.GOOGLE || 'AIzaSyCS6aR9Ini0ffsm6TmMnX3u3GWRVTadsbI'
